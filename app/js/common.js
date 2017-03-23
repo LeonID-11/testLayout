6966372eb2f0b2a506540efe585a1ef73e6a5eae
leonid-11 <rleonidr@gmail.com>
2017-03-23 22:35:09 +0200

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { 
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+
+});
